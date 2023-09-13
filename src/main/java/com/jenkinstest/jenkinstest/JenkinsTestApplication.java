d707a022b112653a455237bcad062d9456632d8b
samsonogberawhe <samsonogberawhe@gmail.com>
2023-09-13 12:33:51 +0100

    added first build server

--- src/main/java/com/jenkinstest/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/jenkinstest/jenkinstest/JenkinsTestApplication.java
@@ -1,19 +1,23 @@
 package com.jenkinstest.jenkinstest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.swing.*;
-
 @SpringBootApplication
 public class JenkinsTestApplication {
 
-    Logger logger = LoggerFactory.getLogger(JenkinsTestApplication.class);
+static Logger logger = LoggerFactory.getLogger(JenkinsTestApplication.class);
+
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
+        logger.error("There is an error here");
+        logger.warn("I am warning you");
+
     }
 
 }
--- src/test/java/com/jenkinstest/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/jenkinstest/jenkinstest/JenkinsTestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jenkinstest.jenkinstest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class JenkinsTestApplicationTests {
 
+    public Logger logger  = LoggerFactory.getLogger(JenkinsTestApplicationTests.class);
     @Test
     void contextLoads() {
+        logger.debug("Test class is running {}", JenkinsTestApplicationTests.class);
+        assertTrue(true);
     }
+
+
 
 }
