a2dbee5321debbac2c5ffedeb3f01195879d5b51
samsonogberawhe <samsonogberawhe@gmail.com>
2023-09-13 22:58:21 +0100

    commited

--- src/main/java/com/jenkinstest/jenkinstest/controller.java
+++ src/main/java/com/jenkinstest/jenkinstest/controller.java
@@ -1,18 +1,14 @@
 package com.jenkinstest.jenkinstest;
 
-import com.fasterxml.jackson.databind.util.JSONWrappedObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.lang.reflect.Method;
 
 @RestController
 public class controller {
 
     @RequestMapping(value = "/getname", method = RequestMethod.GET)
     public String getName(){
-//        return "You have successfully retrieved name";
         return null;
     }
 }
