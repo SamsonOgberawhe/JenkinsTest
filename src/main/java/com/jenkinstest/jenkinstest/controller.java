790420790cdf9d31b5cdfab7c1bc44b0103177cc
samsonogberawhe <samsonogberawhe@gmail.com>
2023-09-14 11:42:25 +0100

    The dev branch is updated

--- src/main/java/com/jenkinstest/jenkinstest/controller.java
+++ src/main/java/com/jenkinstest/jenkinstest/controller.java
@@ -1,14 +1,17 @@
 package com.jenkinstest.jenkinstest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class controller {
 
     @RequestMapping(value = "/getname", method = RequestMethod.GET)
     public String getName(){
-        return null;
+
+        log.info("This is a new line that was added");
+        return "This is the dev branch";
     }
 }
